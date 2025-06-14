9b08267dcaa1889428b6c825135e016cc617d6b6
wakin04 <joaquincarloscasares04@gmail.com>
2025-06-14 15:14:48 +0800

    change settings

--- student_management/student_management/settings.py
+++ student_management/student_management/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-7=)e_iy0t6w1eik4y#!9m2p!&9n&_9s^n(z*m&_i0ykjs@a^6r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['52.66.239.49', '*']
 
 
 # Application definition
